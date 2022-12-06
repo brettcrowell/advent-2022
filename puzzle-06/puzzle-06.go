747b2d6237af2c3b0f938e9f88759389ac1f1746
Brett Crowell <brettcrowell@gmail.com>
2022-12-06 17:21:02 -0500

    puzzle 06, part 1

---
+++ puzzle-06/puzzle-06.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func all_different(chars []string) bool {
+	seen := map[string]bool{}
+
+	for _, char := range chars {
+		if seen[char] {
+			return false
+		}
+		seen[char] = true
+	}
+
+	return true
+}
+
+func main() {
+	input, err := os.Open(os.Args[1])
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer input.Close()
+	scanner := bufio.NewScanner(input)
+	scanner.Split(bufio.ScanRunes)
+
+	iterations := 1
+	trailing := []string{}
+
+	for scanner.Scan() {
+		current := scanner.Text()
+		trailing = append(trailing, current)
+
+		if len(trailing) == 4 {
+			if all_different(trailing) {
+				fmt.Println(iterations)
+				break
+			} else {
+				trailing = trailing[1:]
+			}
+		}
+
+		iterations++
+	}
+
+}
