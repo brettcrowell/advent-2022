c5165314a3959d65094df07dc6c4f0dd6db39964
Brett Crowell <brettcrowell@gmail.com>
2022-12-11 19:55:55 -0500

    puzzle 10, part 2

--- puzzle-10/puzzle-10.go
+++ puzzle-10/puzzle-10.go
@@ -8,13 +8,43 @@ import (
 	"strings"
 )
 
+type crt struct {
+	posn   int
+	pixels []string
+}
+
+func (crt *crt) tick(x int) {
+	pixel := "."
+
+	for p := crt.posn - 1; p < crt.posn+2; p++ {
+		if x == p {
+			pixel = "#"
+		}
+	}
+
+	crt.pixels = append(crt.pixels, pixel)
+	crt.posn = crt.posn%40 + 1
+}
+
+func (crt *crt) render() {
+	for p, pixel := range crt.pixels {
+		fmt.Print(pixel)
+
+		if p%40 == 0 {
+			fmt.Println()
+		}
+	}
+}
+
 type cpu struct {
 	cycle     int
 	registers map[string]int
 	history   []int
+	crt       crt
 }
 
 func (cpu *cpu) tick() {
+	cpu.crt.tick(cpu.registers["X"])
 	cpu.cycle++
 	cpu.history = append(cpu.history, cpu.getSignalStrength())
 }
@@ -47,8 +77,19 @@ type instruction struct {
 	argument  int
 }
 
+func puzzle02(program *[]instruction) {
+	crt := crt{0, []string{}}
+	cpu := cpu{0, map[string]int{"X": 1}, []int{}, crt}
+
+	for _, instruction := range *program {
+		cpu.exec(instruction)
+	}
+
+	cpu.crt.render()
+}
+
 func puzzle01(program *[]instruction) int {
-	cpu := cpu{0, map[string]int{"X": 1}, []int{}}
+	cpu := cpu{0, map[string]int{"X": 1}, []int{}, crt{}}
 
 	for _, instruction := range *program {
 		cpu.exec(instruction)
@@ -93,4 +134,5 @@ func main() {
 	}
 
 	fmt.Println(puzzle01(&program))
+	puzzle02(&program)
 }
