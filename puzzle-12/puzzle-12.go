c79eb0a7de2820268c94eb293c0bcd0d0dfa8ac2
Brett Crowell <brettcrowell@gmail.com>
2022-12-14 21:41:51 -0500

    puzzle 12, part 1

--- puzzle-12/puzzle-12.go
+++ puzzle-12/puzzle-12.go
@@ -3,8 +3,8 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
-	"sort"
 	"strings"
 )
 
@@ -15,22 +15,8 @@ type Node struct {
 	height int
 }
 
-type Nodes []*Node
-
-func (n Nodes) Len() int {
-	return len(n)
-}
-
-func (n Nodes) Swap(i, j int) {
-	n[i], n[j] = n[j], n[i]
-}
-
-func (n Nodes) Less(i, j int) bool {
-	return (*n[j]).height < (*n[i]).height
-}
-
-func includes(path *Nodes, node *Node) int {
-	for s, square := range *path {
+func includes(path []*Node, node *Node) int {
+	for s, square := range path {
 		if node == square {
 			return s
 		}
@@ -38,9 +24,9 @@ func includes(path *Nodes, node *Node) int {
 	return -1
 }
 
-func render(grid *[][]*Node, path *Nodes) {
+func render(grid [][]*Node, path []*Node) {
 	output := ""
-	for _, row := range *grid {
+	for _, row := range grid {
 		for _, col := range row {
 			move := includes(path, col)
 			if move > -1 {
@@ -58,134 +44,133 @@ func render(grid *[][]*Node, path *Nodes) {
 		}
 		output += "\n"
 	}
-	fmt.Print("\033[" + fmt.Sprint(len(*grid)+1) + "A" + output)
+
+	// move cursor up to the 0,0 position in the grid
+	// clear := "\033[" + fmt.Sprint(len(grid)+1) + "A"
+	// output = clear + output
+
+	fmt.Println(output)
 }
 
-func hasVisited(path *Nodes, target *Node) bool {
-	for _, square := range *path {
-		if square == target {
-			return true
-		}
-	}
-	return false
-}
+func getNeighbors(grid [][]*Node, square *Node) []*Node {
+	neighbors := []*Node{}
 
-func getNeighbors(grid *[][]*Node, square *Node) Nodes {
-	neighbors := Nodes{}
-
-	if square.y < len(*grid)-1 {
+	if square.y < len(grid)-1 {
 		// down
-		neighbors = append(neighbors, (*grid)[square.y+1][square.x])
+		neighbors = append(neighbors, (grid)[square.y+1][square.x])
 	}
 
-	if square.x < len((*grid)[0])-1 {
+	if square.x < len((grid)[0])-1 {
 		// right
-		neighbors = append(neighbors, (*grid)[square.y][square.x+1])
+		neighbors = append(neighbors, (grid)[square.y][square.x+1])
 	}
 
 	if square.y > 0 {
 		// up
-		neighbors = append(neighbors, (*grid)[square.y-1][square.x])
+		neighbors = append(neighbors, (grid)[square.y-1][square.x])
 	}
 
 	if square.x > 0 {
 		// left
-		neighbors = append(neighbors, (*grid)[square.y][square.x-1])
+		neighbors = append(neighbors, (grid)[square.y][square.x-1])
 	}
 
 	return neighbors
 }
 
-func filterValid(visited *map[*Node]bool, neighbors *Nodes, target *Node) Nodes {
-	valid := Nodes{}
+func getShortestPath(
+	graph map[*Node][]*Node,
+	start *Node,
+	destination *Node,
+) []*Node {
+	// create a map of distances to each node
+	distances := map[*Node]float64{}
 
-	for _, neighbor := range *neighbors {
-		gap := neighbor.height - (*target).height
-		hasVisited := (*visited)[neighbor]
+	// keep track of all visited nodes
+	visited := map[*Node]bool{}
 
-		isValid := gap < 2 && !hasVisited
+	for node := range graph {
+		distances[node] = math.Inf(1)
+		visited[node] = false
+	}
 
-		if isValid {
-			valid = append(valid, neighbor)
+	// seed distances map with starting node
+	distances[start] = 0
+
+	// keep track of the closest neighbor for each node
+	preceding := map[*Node]*Node{}
+
+	for range graph {
+		distance := math.Inf(1)
+
+		// get next unprocessed node, initially will equal start
+		var next *Node
+
+		for node := range graph {
+			if next != nil {
+				distance = distances[next]
+			}
+
+			if !visited[node] && distances[node] < distance {
+				next = node
+			}
+		}
+
+		// update the node to visited
+		visited[next] = true
+
+		for _, neighbor := range graph[next] {
+			// the distance to this neighbor following a path through "closest" node
+			distance := distances[next] + 1.0
+
+			if distance < distances[neighbor] {
+				// if shorter, accept it
+				distances[neighbor] = distance
+
+				// mark this as the neighbor's preceding node
+				preceding[neighbor] = next
+			}
+		}
+
+		if distances[next] == math.Inf(1) {
+			panic("No path found between nodes")
 		}
 	}
 
-	return valid
+	shortest := []*Node{}
+	node := destination
+
+	for node != start {
+		// reverse engineer the map of neighbors to find the shortest path
+		shortest = append(shortest, preceding[node])
+		node = preceding[node]
+	}
+
+	return shortest
 }
 
-func getPath(
-	grid *[][]*Node,
-	start *Node,
-	destination string,
-) Nodes {
+func puzzle01(grid [][]*Node, graph map[*Node][]*Node) int {
+	var start *Node
+	var end *Node
 
-	visited := map[*Node]bool{}
-
-	// keep track of the path
-	path := Nodes{}
-
-	next := start
-
-	for next != nil {
-
-		// if we've never been here, add it to the list
-		visited[next] = true
-
-		// get all adjacent squares
-		neighbors := getNeighbors(grid, next)
-
-		// filter down to squares which are reachable
-		valid := filterValid(&visited, &neighbors, next)
-
-		if len(valid) == 0 {
-			if next == start {
-				// if we're back at the start with no valid neighbors
-				// we have nowhere to go!
+	for node := range graph {
+		switch (*node).label {
+		case "S":
+			{
+				start = node
 				break
 			}
 
-			visited[path[len(path)-1]] = false
-
-			// roll back the last node
-			path = path[:len(path)-1]
-
-			// if no valid edges, go back one step and try again
-			next = path[len(path)-1]
-
-			continue
-		}
-
-		// keep appending to path
-		path = append(path, next)
-
-		if valid[0].label == destination {
-			// if we're at our destination, return
-			return path
-		}
-
-		// sort reachable nodes by distance
-		sort.Sort(Nodes(valid))
-
-		// check the next valid node
-		next = valid[0]
-	}
-
-	panic("No path found between nodes")
-}
-
-func puzzle01(nodes *[]Node, grid *[][]*Node) int {
-	var start *Node
-
-	for _, node := range *nodes {
-		if node.label == "S" {
-			start = &node
-			break
+		case "E":
+			{
+				end = node
+				break
+			}
 		}
 	}
 
-	path := getPath(grid, start, "E")
-
-	render(grid, &path)
+	path := getShortestPath(graph, start, end)
+	render(grid, path)
 
 	return len(path)
 }
@@ -202,14 +187,13 @@ func main() {
 
 	heights := map[string]int{}
 
-	for c, char := range strings.Split("abcdefghijklmnopqrstuvwxyzE", "") {
+	for c, char := range strings.Split("abcdefghijklmnopqrstuvwxyz", "") {
 		heights[char] = c
 	}
 
 	heights["S"] = heights["a"]
 	heights["E"] = heights["z"]
 
-	squares := []Node{}
 	grid := [][]*Node{}
 
 	r := 0
@@ -217,13 +201,28 @@ func main() {
 		currentRow := []*Node{}
 		for c, col := range strings.Split(scanner.Text(), "") {
 			height := heights[col]
-			squares = append(squares, Node{c, r, col, height})
-			currentRow = append(currentRow, &squares[len(squares)-1])
+			node := Node{c, r, col, height}
+			currentRow = append(currentRow, &node)
 		}
 		grid = append(grid, currentRow)
 		r++
 	}
 
+	graph := map[*Node][]*Node{}
+
+	for _, row := range grid {
+		for _, node := range row {
+			neighbors := getNeighbors(grid, node)
+			for _, neighbor := range neighbors {
+				gap := neighbor.height - node.height
+
+				if gap < 2 {
+					graph[node] = append(graph[node], neighbor)
+				}
+			}
+		}
+	}
+
 	// render(&grid, &Nodes{})
-	fmt.Println(puzzle01(&squares, &grid))
+	fmt.Println(puzzle01(grid, graph))
 }
